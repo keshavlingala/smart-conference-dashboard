b6ceda1504f3f2f1207892aa2470f3f684a8c1a3
ritik <ritik20023@gmail.com>
2020-07-27 15:00:16 +0530

    Change the device component ts,html and css

--- src/app/common/datatable/data-table/data-table.component.ts
+++ src/app/common/datatable/data-table/data-table.component.ts
@@ -1,12 +1,10 @@
-import {Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges, TemplateRef, ViewChild} from '@angular/core';
+import {Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges, ViewChild} from '@angular/core';
 import {ActionChange, DataTableActions, DataTableConfig, Device} from '../../../shared/models/data-table.model';
 import {MatTableDataSource} from '@angular/material/table';
 import {MatPaginator} from '@angular/material/paginator';
 import {MatSort} from '@angular/material/sort';
 import {SelectionModel} from '@angular/cdk/collections';
 import {MatSnackBar} from '@angular/material/snack-bar';
-import {ViewDetailsPopup} from '../../../common/dialog/models/data.model';
-import {Card, Setting} from '../../../common/card-module/models/card.model';
 import {DialogFactoryService} from '../../../core/services/dialog-factory.service';
 
 @Component({
@@ -26,117 +24,7 @@ export class DataTableComponent implements OnChanges, OnInit {
   @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
   @ViewChild(MatSort, {static: true}) sort: MatSort;
   selection = new SelectionModel<Device>(true, []);
-  @ViewChild(TemplateRef) tpl: TemplateRef<any>;
-  @ViewChild('userDialogTemplate')
-  userDialogTemplate: TemplateRef<any>;
-  @Input() popUpData: ViewDetailsPopup = {
-    title: {
-      name: '76jasdbk0920',
-      icon: 'fa-id-badge',
-    },
-    tabs: {
-      attributes: [
-        {
-          Title: 'Humidity',
-          Content: 50.997729349,
-          Footer: new Date().toISOString(),
-        },
-        {
-          Title: 'Lights ON',
-          Content: 0,
-          Footer: new Date().toISOString(),
-        },
-        {
-          Title: 'Occupancy',
-          Content: 5,
-          Footer: new Date().toISOString(),
-        },
-        {
-          Title: 'Projector ON',
-          Content: 36.354,
-          Footer: new Date().toISOString(),
-        },
-        {
-          Title: 'Temperature',
-          Content: 5,
-          Footer: new Date().toISOString(),
-        },
-        {
-          Title: 'Luminosity',
-          Content: 78.323,
-          Footer: new Date().toISOString(),
-        },
-        {
-          Title: 'Prerna',
-          Content: 50.997729349,
-          Footer: new Date().toISOString(),
-        },
-        {
-          Title: 'patraaj ON',
-          Content: 0,
-          Footer: new Date().toISOString(),
-        },
-        {
-          Title: 'nds;oasfkd',
-          Content: 5,
-          Footer: new Date().toISOString(),
-        },
-        {
-          Title: 'mlsandvls ON',
-          Content: 36.354,
-          Footer: new Date().toISOString(),
-        },
-        {
-          Title: 'asdvm s.d',
-          Content: 5,
-          Footer: new Date().toISOString(),
-        },
-      ],
-      actions: [
-        {
-          icon: ' fa-snowflake-o',
-          value: 'Turn ON AC',
-          toggle: false,
-        },
-        {
-          icon: 'fa-lightbulb-o',
-          value: 'Turn ON Lights',
-          toggle: false,
-        },
-        {
-          icon: 'fa-camera',
-          value: 'Projector ON',
-          toggle: false,
-        },
-        {
-          icon: 'fa-thermometer-empty',
-          value: 'Temprature Check',
-          toggle: false,
-        },
-      ],
-    },
-    config: {
-      dismissbtn: true,
-    },
-  };
-  show = false;
-  cardData: Card[];
-  tempData: Card[];
-  @Input() setting: Setting[] = [
-    {
-      Elements_Number: 6,
-      color: 'yellow',
-      icon: 'build',
-      design: 'design3',
-      apipaginator: false,
-    },
-  ];
-  index: number;
-
-//  POp up Model Methods
-  startIndex = 0;
-  endIndex: number = this.setting[0].Elements_Number;
-
+  
   constructor(
     private snack: MatSnackBar,
     private dialogFactoryService: DialogFactoryService
@@ -147,8 +35,6 @@ export class DataTableComponent implements OnChanges, OnInit {
     if (!this.config.checkbox) {
       this.displayedColumns.shift();
     }
-    this.tempData = this.popUpData.tabs.attributes;
-    this.cardData = this.setting[0].apipaginator === false ? this.tempData.slice(this.startIndex, this.endIndex) : this.tempData;
   }
 
   ngOnChanges(changes: SimpleChanges): void {
@@ -192,46 +78,10 @@ export class DataTableComponent implements OnChanges, OnInit {
   }
 
   rowAction(row: Device, action: 'action' | 'bulk-action', name: string): void {
-    if (action === 'action' && name === 'analytics') {
-      console.log(this.userDialogTemplate);
-      this.dialogFactoryService.open(
-        {
-          template: this.userDialogTemplate,
-        },
-        {width: 500, height: 605, disableClose: true}
-      );
-    }
     this.actionClick.emit({
       selected: row,
       type: action,
       name
     });
-  }
-
-  slideToggleFunction(id: number): void {
-    this.popUpData.tabs.actions[id].toggle = !this.popUpData.tabs.actions[id].toggle;
-  }
-
-  accessKeyShow(): void {
-    this.show = !this.show;
-  }
-
-  moveBackward(): void {
-    if (this.startIndex !== 0) {
-      this.startIndex = this.startIndex - this.setting[0].Elements_Number;
-      this.endIndex = this.endIndex - this.setting[0].Elements_Number;
-      this.cardData = this.setting[0].apipaginator === false ? this.tempData.slice(this.startIndex, this.endIndex) : this.tempData;
-      this.tempData = this.popUpData.tabs.attributes;
-    }
-  }
-
-  moveForward(): void {
-    length = this.tempData.length;
-    if (this.endIndex < length) {
-      this.startIndex = this.startIndex + this.setting[0].Elements_Number;
-      this.endIndex = this.endIndex + this.setting[0].Elements_Number;
-      this.cardData = this.setting[0].apipaginator === false ? this.tempData.slice(this.startIndex, this.endIndex) : this.tempData;
-      this.tempData = this.popUpData.tabs.attributes;
-    }
   }
 }
