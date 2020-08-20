a51c7f46e69d1762bdf60dc6a11a6088ea252370
Keshav Reddy <keshavlingala@gmail.com>
2020-08-20 17:49:36 +0530

    Style changed to scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {SideNavigationService} from './core/services/side-navigation.service';
-import {NavItem, GroupedNavItem} from './common/side-navigation/models/side-nav-models';
+import {GroupedNavItem, NavItem} from './common/side-navigation/models/side-nav-models';
 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit  {
-  public heading:string = "Cognisense";
-  public showLogoutBtn:boolean = true;
-  public navItems:(NavItem|GroupedNavItem)[] = [];
-  constructor(private __sideNavService: SideNavigationService){
+export class AppComponent implements OnInit {
+  public heading: string = "Cognisense";
+  public showLogoutBtn: boolean = true;
+  public navItems: (NavItem | GroupedNavItem)[] = [];
+
+  constructor(private __sideNavService: SideNavigationService) {
 
   }
 
-  ngOnInit(){
-    this.__sideNavService.getNavItems().subscribe(data=>this.navItems = data);
+  ngOnInit() {
+    this.__sideNavService.getNavItems().subscribe(data => this.navItems = data);
   }
- 
+
 }
