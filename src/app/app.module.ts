8e698ccfadca253bb9c7b7a76099d04c6ce31094
Keshav <keshavlingala@gmail.com>
2020-09-11 12:27:06 +0530

    Generate Keys Card Integrated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,7 +23,6 @@ import {DevicesTypeComponent} from './components/devices-type/devices-type.compo
 import {DevicesGroupComponent} from './components/devices-group/devices-group.component';
 import {RulesComponent} from './components/rules/rules.component';
 import {OtaUpdatesComponent} from './components/ota-updates/ota-updates.component';
-import {GenerateKeysComponent} from './components/generate-keys/generate-keys.component';
 import {DashboardComponent} from './components/dashboard/dashboard.component';
 import {MembersComponent} from './components/members/members.component';
 import {Dummy1Component} from './components/dummy1/dummy1.component';
@@ -40,7 +39,6 @@ import {DataViewModule} from './common/data-view/data-view.module';
     DevicesGroupComponent,
     RulesComponent,
     OtaUpdatesComponent,
-    GenerateKeysComponent,
     DashboardComponent,
     MembersComponent,
     Dummy1Component,
