c96e574c3a0a39e06167857ebba6df4043228ec4
Keshav <keshavlingala@gmail.com>
2020-09-12 18:05:50 +0530

    Final Commit

--- src/app/featured-modules/ota-updates/ota-base-page/ota-base-page.component.ts
+++ src/app/featured-modules/ota-updates/ota-base-page/ota-base-page.component.ts
@@ -1,7 +1,8 @@
-import { deviceTypes } from './../../../shared/datagenerator/datagenerator.dev';
-import { Card } from './../../../common/card-module/models/card.model';
-import { Component, OnInit } from '@angular/core';
-import { OtaServiceService } from '../ota-service.service';
+import {Card} from './../../../common/card-module/models/card.model';
+import {Component, OnInit} from '@angular/core';
+import {OtaServiceService} from '../ota-service.service';
+import {FormBuilder, FormGroup} from "@angular/forms";
+import {BehaviorSubject} from "rxjs";
 
 @Component({
   selector: 'app-ota-base-page',
@@ -10,16 +11,24 @@ import { OtaServiceService } from '../ota-service.service';
 })
 export class OtaBasePageComponent implements OnInit {
   cardData: Card[];
-  deviceTypes= deviceTypes;
   selectedCards: Card[];
   selected: string;
+  deviceSearch: FormGroup;
+  deviceTypes = new BehaviorSubject<string[]>([]);
+  allDeviceTypes: string[];
+
   constructor(
-    private otaService: OtaServiceService
+    private otaService: OtaServiceService,
+    private fb: FormBuilder
   ) {
 
   }
 
   async ngOnInit() {
+    this.deviceSearch = this.fb.group({
+      deviceType: ['']
+    })
+    this.allDeviceTypes = this.otaService.getDeviceTypes();
     this.cardData = (await this.otaService.getOtaResponse().toPromise()).data.ota.map((ota) => {
       return {
         Content: ota.deviceType,
@@ -27,6 +36,14 @@ export class OtaBasePageComponent implements OnInit {
         Title: ota.ota.otaName,
       };
     });
+    this.deviceTypes.next(this.allDeviceTypes);
+    this.deviceSearch.valueChanges.subscribe(change => {
+      console.log(change);
+      let key = this.deviceSearch.value.deviceType;
+      this.deviceTypes.next(this.allDeviceTypes.filter(name => {
+        return name.includes(key);
+      }))
+    })
   }
 
   onSelect(value: any) {
--- src/app/featured-modules/ota-updates/ota-service.service.ts
+++ src/app/featured-modules/ota-updates/ota-service.service.ts
@@ -1,24 +1,32 @@
-import { otaUpdatesGenerator } from './../../shared/datagenerator/datagenerator.dev';
-import { Observable,of } from 'rxjs';
-import { Injectable } from '@angular/core';
-import { Card } from 'src/app/common/card-module/models/card.model';
-import { delay } from 'rxjs/internal/operators';
-import { OtaResponse } from './ota.model';
+import {deviceTypes, otaUpdatesGenerator} from './../../shared/datagenerator/datagenerator.dev';
+import {Observable, of} from 'rxjs';
+import {Injectable} from '@angular/core';
+import {Card} from 'src/app/common/card-module/models/card.model';
+import {delay} from 'rxjs/internal/operators';
+import {OtaResponse} from './ota.model';
+
 @Injectable({
   providedIn: 'root'
 })
 export class OtaServiceService {
-  cardData : Card[];
-  constructor() {
-   }
+  cardData: Card[];
 
-   getCards(): Observable<Card[]>{
-     return of(this.cardData).pipe(delay(2000));
-   }
-   getData(){
+  constructor() {
+  }
+
+  getCards(): Observable<Card[]> {
+    return of(this.cardData).pipe(delay(2000));
+  }
+
+  getData() {
     return this.cardData;
-   }
-   getOtaResponse(): Observable<OtaResponse> {
+  }
+
+  getOtaResponse(): Observable<OtaResponse> {
     return of(otaUpdatesGenerator(300)).pipe(delay(700));
+  }
+
+  getDeviceTypes() {
+    return deviceTypes
   }
 }
